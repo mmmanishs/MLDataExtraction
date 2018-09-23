740940407d82b19385ab5720df73e230bdd7454f
Manish Singh <mmmanishs@gmail.com>
2018-09-23 19:51:45 -0400

    Added diagram

--- MLDataExtraction/AppDelegate.swift
+++ MLDataExtraction/AppDelegate.swift
@@ -14,11 +14,7 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSTextFieldDelegate {
     @IBOutlet weak var window: NSWindow!
     @IBOutlet weak var inputFolderTextField: NSTextField!
     @IBOutlet weak var outputFolderTextField: NSTextField!
-    
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
-    }
-    
+
     @IBAction func startButtonClicked(_ sender: Any) {
         // Start off things here. Good luck & godspeed
         guard let moviePathCommaSeperated = inputFolderTextField.cell?.title,
@@ -34,10 +30,6 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSTextFieldDelegate {
                                       moviePath: moviePaths,
                                       outputPath: outputPath)
         startExtraction(with: appArgument)
-    }
-    
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Probably can delete stuff here
     }
 }
 
